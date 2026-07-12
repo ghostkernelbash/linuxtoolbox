076dfc8dad2b80999909f23b3cc13d73d1ad454d
Robert Ethridge <ghostkernelbash@gmail.com>
2026-07-12 12:15:47 -0500

    Version 0.1 - Initial Linux Toolbox

---
+++ toolbox.sh
@@ -0,0 +1,69 @@
+#!/bin/bash
+
+while true
+do
+    clear
+    echo 
+    echo 
+    echo "==============================="
+    echo " Robert's Linux Toolbox v0.3"
+    echo "==============================="
+    echo
+    echo "1) Show Date"
+    echo "2) Show Current Directory"
+    echo "3) List Files"
+    echo "4) Exit"
+    echo "5) Who am I?"
+    echo "6) Show free disk space"
+    echo "7) Show memory usage"
+    echo "8) Show CPU Information"
+
+    read -p "Choose an option: " choice
+
+    case $choice in
+        1)
+            date
+            ;;
+        2)
+            pwd
+            ;;
+        3)
+            ls -lh
+            ;;
+        4)
+            echo "Goodbye!"
+            ;;
+
+        5)  echo "Whoami"
+
+            ;;
+
+        6)  df -h
+
+            ;;
+
+        7) free -g
+            ;;
+        8)
+        echo
+        echo "CPU Information"
+        echo "---------------"
+        lscpu
+        echo
+        read -p "Press Enter to continue..."
+        ;;
+       
+            4)
+        echo "Goodbye!"
+        exit
+        ;;
+
+    *)
+        echo "Invalid choice."
+        ;;
+            
+    esac
+
+    echo
+    read -p "press enter to continue... " choice
+done
